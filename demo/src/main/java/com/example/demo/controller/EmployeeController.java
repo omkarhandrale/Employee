e344287a8ff89f653450ecc1063fbb8a98dac645
Handrale <omkar.handrale@accenture.com>
2023-02-24 20:46:34 +0530

    added get by id endpoint

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,7 +42,6 @@ public class EmployeeController
     
     @RequestMapping(value="/create", method=RequestMethod.POST)
     public Employee createEmployee(@RequestBody Employee emp) {
-   
     	 return empservice.createEmployee(emp);
     	
     	
@@ -49,5 +50,14 @@ public class EmployeeController
     @GetMapping("/getall")
     public List<Employee> allEmployeeDetails(){
     	 return (repo.findAll());
+    }
+    
+    @GetMapping("/getByid/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+    	Optional<Employee> emp=repo.findById(id);
+    	if(emp.isPresent()) {
+    		return emp.get();
+    	}
+    	return null;
     }
 }
